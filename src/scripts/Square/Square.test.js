5e57c4fc1811d8ec0dc9fb21d74fa81c33655c66
Francis Langit <francis.villanueva.langit@gmail.com>
2021-07-31 22:35:27 +0800

    Add test for Square.coordinates in Square.test.js

--- src/scripts/Square/Square.test.js
+++ src/scripts/Square/Square.test.js
@@ -1,7 +1,7 @@
 import { Square } from "./square";
 
 
-test('Object returned by Square() factory function', () => {
+test('Object returned by calling Square(5, 8)', () => {
     const testSquare = Square(5,8);
     const expectedSquare = {
         'coordinates': [5, 8],
@@ -9,4 +9,10 @@ test('Object returned by Square() factory function', () => {
         'isHit': false,
     } 
     expect(testSquare).toEqual(expectedSquare);
+});
+
+test('Square.coordinates of return value of Square(2, 6)', () => {
+    const testSquare = Square(2, 6);
+    const expectedCoordinates = [2, 6];
+    expect(testSquare.coordinates).toEqual(expectedCoordinates);
 });
