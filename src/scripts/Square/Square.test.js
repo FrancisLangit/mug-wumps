c5b1ad925d1a0916a1c02f5bfb7ee85ea4659304
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-01 14:30:16 +0800

    Refactor test cases in Square.test.js

--- src/scripts/Square/Square.test.js
+++ src/scripts/Square/Square.test.js
@@ -1,18 +1,14 @@
 import { Square } from "./square";
 
 
-test('Object returned by calling Square(5, 8)', () => {
-    const testSquare = Square(5,8);
-    const expectedSquare = {
-        'coordinates': [5, 8],
-        'isOccupied': false,
-        'isHit': false,
-    } 
-    expect(testSquare).toEqual(expectedSquare);
+test('Properties of object returned by Square(5, 8)', () => {
+    const square = Square(5,8);
+    expect(square).toHaveProperty('coordinates', [5, 8]);
+    expect(square).toHaveProperty('isOccupied', false);
+    expect(square).toHaveProperty('isHit', false);
 });
 
-test('Square.coordinates of return value of Square(2, 6)', () => {
+test('"coordinates" property of return value of Square(2, 6)', () => {
     const testSquare = Square(2, 6);
-    const expectedCoordinates = [2, 6];
-    expect(testSquare.coordinates).toEqual(expectedCoordinates);
+    expect(testSquare).toHaveProperty('coordinates', [2,6]);
 });
