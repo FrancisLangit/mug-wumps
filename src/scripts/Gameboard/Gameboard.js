f8aa6fbf8d03a685cf76da93cb3581c5d84ff5c4
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-03 20:30:13 +0800

    Configure Gameboard.isAllShipsSunk() to pass tests

--- src/scripts/Gameboard/Gameboard.js
+++ src/scripts/Gameboard/Gameboard.js
@@ -2,15 +2,24 @@ const Gameboard = () => {
     let ships = [];
     let misses = [];
     
+    
     const receiveAttack = () => {
         return undefined;
     }
 
+    /**
+     * Returns `true` if `isSunk()` method of all `Ship` objects in `ships`
+     * array return `true`.
+     * 
+     * @returns {boolean} Boolean dependent on status of ships.
+     */
     const isAllShipsSunk = () => {
-        return undefined;
+        return ships.every((ship) => {
+            return ship.isSunk() === true;
+        }); 
     }
 
-    return { receiveAttack, isAllShipsSunk, ships, misses }
+    return { ships, misses, receiveAttack, isAllShipsSunk }
 }
 
 
