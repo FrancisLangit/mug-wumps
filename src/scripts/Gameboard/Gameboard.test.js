e8a03d3c7e2aeff7b814e8b993fcecf95f881993
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-03 20:20:17 +0800

    Write test for Gameboard.isAllShipsSunk() method

--- src/scripts/Gameboard/Gameboard.test.js
+++ src/scripts/Gameboard/Gameboard.test.js
@@ -9,3 +9,12 @@ test('Properties of object returned by Gameboard()', () => {
     expect(gameboard).toHaveProperty('isAllShipsSunk');
 });
 
+test('isAllShipsSunk() method of object returned by Gameboard()', () => {
+    // Factory function returning a mock of a Ship object.
+    const _getMockShip = () => { const isSunk = () => true }
+    // Initialize instance of Gameboard and fill its ships array with mocks.
+    let gameboard = Gameboard();
+    gameboard.ships = Array(5).fill(_getMockShip());
+    // Test isAllShipsSunk() method of gameboard instance.
+    expect(gameboard.isAllShipsSunk()).toEqual(true);
+});
