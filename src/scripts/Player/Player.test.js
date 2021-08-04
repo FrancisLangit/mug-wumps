6aa4ddcdf34659a331b0d10e19c16a49540f0cf1
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-04 16:20:42 +0800

    Write tests for properties of Player object

--- src/scripts/Player/Player.test.js
+++ src/scripts/Player/Player.test.js
@@ -1,1 +1,12 @@
 import { Player } from "./Player";
+
+
+test('Properties of object from Player(enemyGameboard)', () => {
+    const player = Player('mockEnemyGameboard');
+    expect(player).toHaveProperty('makeAttack');
+});
+
+test('Properties of object from Player(enemyGameboard, true)', () => {
+    const computer = Player('mockEnemyGameboard', true);
+    expect(computer).toHaveProperty('makeRandomAttack');
+});
