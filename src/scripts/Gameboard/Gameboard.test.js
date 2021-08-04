36a1d637cfca9db761a0d749c538ee7fa87f4e4e
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-04 15:18:52 +0800

    Add tests for Gameboard() addShip() method

--- src/scripts/Gameboard/Gameboard.test.js
+++ src/scripts/Gameboard/Gameboard.test.js
@@ -6,19 +6,26 @@ test('Properties of object returned by Gameboard()', () => {
     const gameboard = Gameboard();
     expect(gameboard).toHaveProperty('ships');
     expect(gameboard).toHaveProperty('misses', []);
+    expect(gameboard).toHaveProperty('addShip');
     expect(gameboard).toHaveProperty('receiveAttack');
     expect(gameboard).toHaveProperty('isAllShipsSunk');
+});
+
+test('addShip() method of object returned by Gameboard()', () => {
+    let gameboard = Gameboard();
+    let ship = Ship(0, 0, 2);
+    gameboard.addShip(ship);
+    expect(gameboard.ships[0]).toEqual(ship);
 });
 
 test('receiveAttack() of Gameboard() making hit on ships', () => {
     // Initialize Gameboard object and fill its ships array.
     let gameboard = Gameboard()
-    gameboard.ships.push(Ship(0, 0, 5), Ship(5, 5, 3, true));
-    // Render attacks that make hits.
-    gameboard.receiveAttack(1, 0);
+    gameboard.addShip(Ship(0, 0, 5));
+    gameboard.addShip(Ship(5, 5, 3, true));
+    // Render attacks that makes hit.
     gameboard.receiveAttack(5, 7);
-    // Expect attack to be rendered on targeted ships.
-    expect(gameboard.ships[0].hits[1]).toEqual(true);
+    // Expect attack to be rendered on targeted ship.
     expect(gameboard.ships[1].hits[2]).toEqual(true);
     // Expect misses property of gameboard to remain empty.
     expect(gameboard.misses).toEqual([]);
