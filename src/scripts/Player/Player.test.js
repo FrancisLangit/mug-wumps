f2977f6316a26426be7580b848412f954573aa0f
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-05 14:47:12 +0800

    Add test for Player object makeAttack() method

--- src/scripts/Player/Player.test.js
+++ src/scripts/Player/Player.test.js
@@ -1,4 +1,6 @@
+import { Gameboard } from "../Gameboard/Gameboard";
 import { Player } from "./Player";
+import { Ship } from "../Ship/Ship";
 
 
 test('Properties of object from Player(enemyGameboard)', () => {
@@ -9,4 +11,20 @@ test('Properties of object from Player(enemyGameboard)', () => {
 test('Properties of object from Player(enemyGameboard, true)', () => {
     const computer = Player('mockEnemyGameboard', true);
     expect(computer).toHaveProperty('makeRandomAttack');
+});
+
+test('makeAttack() method of object from Player(enemyGameboard)', () => {
+    // Initialize enemyGameboard and Player objects to test.
+    let enemyGameboard = Gameboard();
+    enemyGameboard.addShip(Ship(0, 1, 3));
+
+    let player = Player(enemyGameboard);
+    
+    // Call player.makeAttack() twice. One for hit, one for miss. 
+    player.makeAttack(1, 1);
+    player.makeAttack(5, 5);
+    
+    // Assert that targeted ship hit and miss recorded.
+    expect(enemyGameboard.ships[0].hits[1]).toEqual(true);
+    expect(enemyGameboard.misses[0]).toEqual([5, 5]);
 });
