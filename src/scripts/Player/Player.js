148d0071b6edfb29b354db16b0e25e68ebcef277
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-06 15:16:38 +0800

    Configure Player.makeRandomAttack() to pass tests

--- src/scripts/Player/Player.js
+++ src/scripts/Player/Player.js
@@ -1,6 +1,40 @@
 const Player = (enemyGameboard, isComputer) => {
+    const _isPositionHit = (x, y) => {
+        let isHit = false
+        enemyGameboard.ships.forEach((ship) => {
+            ship.getPositions().forEach((position) => {
+                const isTarget = position.toString() === [x, y].toString();
+                const isPositionHit = position === true
+                if (isTarget && isPositionHit) {
+                    isHit = true;
+                }
+            });
+        });
+        return isHit;
+    }
+
+    const _isPositionMiss = (x, y) => {
+        let isMiss = false
+        enemyGameboard.misses.forEach((miss) => {
+            if ([x, y].toString() === miss.toString()) {
+                isMiss = true;
+            }
+        });
+        return isMiss;
+    }
+    
     const makeRandomAttack = () => {
-        return undefined;
+        let choices = []
+        for (let x = 0; x < 10; x++) {
+            for (let y = 0; y < 10; y++) {      
+                if (!_isPositionHit(x, y) && !_isPositionMiss(x, y)) {
+                    choices.push([x, y]);
+                }
+            }
+        }
+        const choiceIndex = Math.floor(Math.random() * choices.length);
+        const choice = choices[choiceIndex];
+        enemyGameboard.receiveAttack(choice[0], choice[1]);
     }
 
     const makeAttack = (x, y) => {
