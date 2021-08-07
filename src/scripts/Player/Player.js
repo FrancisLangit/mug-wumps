0a2fb46ded509216d7d8c070625c6334d6646149
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-07 18:56:16 +0800

    Add documentation to Player.js

--- src/scripts/Player/Player.js
+++ src/scripts/Player/Player.js
@@ -1,5 +1,25 @@
-const Player = (enemyGameboard) => {
+/**
+ * Module containing and exporting `Player` factory function.
+ * 
+ * @module Player
+ */
 
+/**
+ * Returns a `Player` object that can be controlled by the user.
+ * 
+ * @namespace
+ * 
+ * @param {Object} enemyGameboard `Gameboard` object of opponent. 
+ * 
+ * @returns {Object} Player object.
+ */
+const Player = (enemyGameboard) => {
+    /**
+     * Attacks the Player's enemy gameboard on specified coordinates.
+     * 
+     * @param {int} x X-coordinate of attack.
+     * @param {int} y Y-coordinate of attack.
+     */
     const makeAttack = (x, y) => {
         enemyGameboard.receiveAttack(x, y);
     }
