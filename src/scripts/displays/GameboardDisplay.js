bb749940ae752b1ae0addd9f3bd0fc4384a2bb40
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-25 19:08:04 +0800

    Refactor GameboardDisplay() function

--- src/scripts/displays/GameboardDisplay.js
+++ src/scripts/displays/GameboardDisplay.js
@@ -12,18 +12,37 @@ import { GameboardCellDisplay } from "./GameboardCellDisplay";
  * @returns {HTMLElement} `div` rendering a gameboard.
  */
 const GameboardDisplay = (gameboard, isComputer) => {
-    let grid = document.createElement('div');
-    grid.classList.add('gameboard');
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
-            grid.append(GameboardCellDisplay(x, y, gameboard, isComputer));
-        }
-    };
+    /**
+     * Returns a styled `div` representing a gameboard cell.
+     * 
+     * @param {int} x X-coordinate of the cell.
+     * @param {int} y Y-coordinate of the cell.
+     * 
+     * @returns {HTMLElement}
+     */
+    const _getCell = (x, y) => {
+        return GameboardCellDisplay(x, y, gameboard, isComputer);
+    }
 
-    let container = document.createElement('div');
-    container.appendChild(grid);
+    /**
+     * Returns a styled interface representation of the gameboard grid.
+     * 
+     * @returns {HTMLElement}
+     */
+    const _getGrid = () => {
+        let grid = document.createElement('div');
+        grid.classList.add('gameboard');
+        for (let x = 0; x < 10; x++) {
+            for (let y = 0; y < 10; y++) {
+                grid.append(_getCell(x, y));
+            }
+        };
+        return grid;
+    }
 
-    return container;
+    let gameboardDisplay = document.createElement('div');
+    gameboardDisplay.appendChild(_getGrid());
+    return gameboardDisplay;
 }
 
 
