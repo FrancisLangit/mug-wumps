eb015f3bdb61fde46625ed83057de2a163fee6bd
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-25 18:36:55 +0800

    Add documentation to UserInterface.js

--- src/scripts/UserInterface.js
+++ src/scripts/UserInterface.js
@@ -2,9 +2,23 @@ import { Game } from './objects/Game';
 import { GameboardDisplay } from './displays/GameboardDisplay';
 
 
+/**
+ * Immediately invoked function expression (IIFE) module holding methods for 
+ * the updating of the user interface.
+ * 
+ * @module UserInterface
+ */
 const UserInterface = (() => {
     const root = document.getElementById('root');
 
+    /**
+     * Private function returning `div` holding display of both user and
+     * computer gameboard.
+     * 
+     * @memberof module:UserInterface
+     * 
+     * @returns {HTMLElement} `div` holding game's gameboards.
+     */
     const _getGameboards = () => {
         const gameboardsContainer = document.createElement('div');
         gameboardsContainer.append(
@@ -14,10 +28,24 @@ const UserInterface = (() => {
         return gameboardsContainer;
     }
 
+    /**
+     * Fills up the "`root`" `div` of the user interface.
+     * 
+     * @memberof module:UserInterface
+     * 
+     * @returns {undefined}
+     */
     const setUp = () => {
         root.appendChild(_getGameboards());
     }
 
+    /**
+     * Refreshes the user interface with new displays of game's objects.
+     * 
+     * @memberof module:UserInterface
+     * 
+     * @returns {undefined}
+     */
     const update = () => {
         root.innerHTML = '';
         setUp();
