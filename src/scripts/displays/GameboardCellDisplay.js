7c5565347c4d56ac4cab8d9025b1caa45f1abb89
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-25 18:49:31 +0800

    Add documentation to GameboardCellDisplay.js

--- src/scripts/displays/GameboardCellDisplay.js
+++ src/scripts/displays/GameboardCellDisplay.js
@@ -1,14 +1,30 @@
 import { UserInterface } from "../UserInterface";
 
 
+/**
+ * Returns a styled `div` representing a gameboard cell.
+ * 
+ * @namespace
+ * 
+ * @param {int} x Coordinate of cell on x-axis of gameboard.
+ * @param {int} y Coordinate of cell on y-axis of gameboard.
+ * @param {Object} gameboard Parent `Gameboard` object of cell to render.
+ * @param {boolean} isComputer `true` if parent `Gameboard` is computer's.
+ *  
+ * @returns {HTMLElement} Styled `div` representing gameboard cell.
+ */
 const GameboardCellDisplay = (x, y, gameboard, isComputer) => {
+    /**
+     * Returns array of the cell's CSS classes in strings.
+     * 
+     * @returns {Array} Holds cell's CSS classes.
+     */
     const _getClasses = () => {
         let classes = ['cell'];
 
         if (isComputer) {
             classes.push('cell-computer');
         }
-
         if (gameboard.isPositionMiss(x, y)) {
             classes.push('cell-miss');
         } else if (gameboard.isPositionShip(x, y, true)) {
@@ -20,6 +36,11 @@ const GameboardCellDisplay = (x, y, gameboard, isComputer) => {
         return classes;
     }
 
+    /**
+     * Renders an attack onto the cell and updates its display.
+     * 
+     * @returns {undefined}
+     */
     const _renderAttack = () => {
         gameboard.receiveAttack(x, y);
         UserInterface.update();
