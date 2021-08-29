3a6a1068a460741c19392398c04d08704de24b95
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-29 15:02:04 +0800

    Add test for RandomShip method length argument

--- src/scripts/objects/Ship/RandomShip.test.js
+++ src/scripts/objects/Ship/RandomShip.test.js
@@ -1,14 +1,23 @@
 import { RandomShip } from "./Ship";
 
 
+const randomShips = [
+    RandomShip(5),
+    RandomShip(4),
+    RandomShip(3),
+    RandomShip(3),
+    RandomShip(2),
+];
+
+test('Length of Ships returned by RandomShip matches arguments', () => {
+    expect(randomShips[0].length).toBe(5);
+    expect(randomShips[1].length).toBe(4);
+    expect(randomShips[2].length).toBe(3);
+    expect(randomShips[3].length).toBe(3);
+    expect(randomShips[4].length).toBe(2);
+});
+
 test('Coordinates of RandomShip(5) return object not beyond grid', () => {
-    const randomShips = [
-        RandomShip(5),
-        RandomShip(4),
-        RandomShip(3),
-        RandomShip(3),
-        RandomShip(2),
-    ];
     for (let i = 0; i < randomShips.length; i++) {
         const ship = randomShips[i];
         const testCoordinate = ship.isVertical ? ship.y : ship.x;
