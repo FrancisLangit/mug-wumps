6b0893d288e2e80393ac951c90115743e59460ca
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-30 15:27:56 +0800

    Create .randomize-button CSS class

--- src/scripts/displays/RandomizeButton.js
+++ src/scripts/displays/RandomizeButton.js
@@ -3,7 +3,10 @@ import { UserInterface } from "../UserInterface";
 
 const RandomizeButton = (gameboard) => {
     const randomizeButton = document.createElement('div');
+    
     randomizeButton.textContent = 'Randomize';
+    randomizeButton.classList.add('randomize-button');
+    
     randomizeButton.addEventListener('click', () => {
         gameboard.reset();
         UserInterface.update();
