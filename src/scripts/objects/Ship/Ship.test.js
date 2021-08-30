24fb9c5702b04f5a68bc1d4aa9db7cc9bda8ab46
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-30 14:43:35 +0800

    Refactor test in Ship.test.js

--- src/scripts/objects/Ship/Ship.test.js
+++ src/scripts/objects/Ship/Ship.test.js
@@ -16,18 +16,9 @@ test('Properties of object returned by Ship(5, 6, 3)', () => {
     expect(ship).toHaveProperty('reset');
 });
 
-test('Properties of object returned by Ship(1, 1, 5, true)', () => {
+test('isVertical of object returned by Ship(1, 1, 5, true) is true', () => {
     const ship = Ship(1, 1, 5, true);
-    expect(ship).toHaveProperty('x', 1);
-    expect(ship).toHaveProperty('y', 1);
-    expect(ship).toHaveProperty('length', 5);
     expect(ship).toHaveProperty('isVertical', true);
-    expect(ship).toHaveProperty('hits', [false, false, false, false, false]);
-
-    expect(ship).toHaveProperty('hit');
-    expect(ship).toHaveProperty('isSunk');
-    expect(ship).toHaveProperty('getPositions');
-    expect(ship).toHaveProperty('reset');
 });
 
 test('hit() method of object returned by Ship(2, 3, 2)', () => {
