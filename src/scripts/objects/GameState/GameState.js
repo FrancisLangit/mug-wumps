0318840bac72b413049528b20ae3aeb7743ebd4e
Francis Langit <francis.villanueva.langit@gmail.com>
2021-08-31 18:40:35 +0800

    Construct GameState module passing tests

--- src/scripts/objects/GameState/GameState.js
+++ src/scripts/objects/GameState/GameState.js
@@ -1,7 +1,45 @@
+/**
+ * Module holding modules that control the running state of the game.
+ * 
+ * @module GameState
+ */
 const GameState = (() => {
-    
+    let running = false;
 
-    return { undefined }
+    /**
+     * Returns the value of GameState's `running` variable.
+     * 
+     * @memberof module:GameState
+     * 
+     * @returns {undefined}
+     */
+    const isRunning = () => {
+        return running;
+    }
+
+    /**
+     * Sets `running` to `true`.
+     * 
+     * @memberof module:GameState
+     * 
+     * @returns {undefined}
+     */
+    const start = () => {
+        running = true;
+    }
+
+    /**
+     * Sets `running` to `false`.
+     */
+    const reset = () => {
+        running = false;
+    }
+
+    return { 
+        isRunning,
+        start,
+        reset,   
+    }
 })();
 
 
